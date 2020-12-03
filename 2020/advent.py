e81a61dc72bf88ee65189bcb4be4a9926d642847
James Benson <jbenson2029@gmail.com>
2020-12-03 22:46:06 +0000

    timer

--- 2020/advent.py
+++ 2020/advent.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 
 import argparse, importlib, inputs
+from datetime import datetime
 
 def import_solver(day, part):
   try:
     return importlib.import_module(f'solvers.advent_{day.zfill(2)}_{part}')
   except ImportError:
     raise Exception(f'Cannot find solver for day {day} part {part}')
+
+def print_table(lines):
+  width = max(map(lambda line: len(line), lines))
+  separator = ''.ljust(width, '-') + '\n'
+  print(separator.join([ line + '\n' for line in lines ]), end='')
 
 if __name__ == "__main__":
   argp = argparse.ArgumentParser()
@@ -16,7 +22,12 @@ if __name__ == "__main__":
   
   try:
     solver = import_solver(args.day, args.part)
+    time_start = datetime.now()
     output = solver.solve(inputs.get_input(args.day))
-    print(output)
+    time_end = datetime.now()
+    print_table([
+      str(output),
+      f'solved in {str(time_end - time_start)}'
+    ])
   except Exception as e:
     print(str(e))
