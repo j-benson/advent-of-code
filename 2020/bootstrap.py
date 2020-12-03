0d7c1eee3da0002490b69232b2dcfa34b173ec9c
James Benson <jbenson2029@gmail.com>
2020-12-03 22:25:41 +0000

    bootstrapper

---
+++ 2020/bootstrap.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import argparse, os
+
+def create_file(file, contents):
+  if not os.path.exists(file):
+    with open(file, 'w') as out:
+      out.write(contents)
+
+if __name__ == "__main__":
+  argp = argparse.ArgumentParser()
+  argp.add_argument('day')
+  args = argp.parse_args()
+
+  module_base = f'advent_{args.day.zfill(2)}'
+  module_solvers = [
+    f'{module_base}_1',
+    f'{module_base}_2',
+  ]
+  file_base = 'solvers/'
+  file_solvers = [ f'{file_base}{module}.py' for module in module_solvers ]
+  file_test = f'{file_base}{module_base}_test.py'
+
+  for f in file_solvers:
+    create_file(
+      f,
+'''
+def solve(input):
+  pass
+
+'''
+    )
+
+  create_file(
+    file_test,
+f'''import {", ".join(module_solvers)}
+
+def test_1():
+  pass
+
+def test_2():
+  pass
+
+'''
+  )
