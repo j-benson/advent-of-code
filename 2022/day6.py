f70147de47ddfe7a3d8f18f7b2a987d7527f4942
James Benson <jbenson2029@gmail.com>
2022-12-06 08:58:49 +0000

    day6.2

--- 2022/day6.py
+++ 2022/day6.py
@@ -2,15 +2,17 @@
 
 import input
 
-if __name__ == '__main__':
-  data = input.as_list()[0]
+def find_marker(marker_length: int, data: str):
   last_chars = []
   for i in range(len(data)):
     c = data[i]
     last_chars.append(c)
-    if (len(last_chars) > 4):
+    if (len(last_chars) > marker_length):
       last_chars.pop(0)
-    if len(set(last_chars)) == 4:
-      print(i + 1)
-      exit()
-    
+    if len(set(last_chars)) == marker_length:
+      return i + 1
+
+if __name__ == '__main__':
+  data = input.as_list()[0]
+  print(find_marker(4, data))
+  print(find_marker(14, data))
